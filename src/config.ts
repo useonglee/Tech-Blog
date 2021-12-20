b91986cf606e3628b15c013ffa21401bf244b502
uslee <uslee@maxst.com>
2021-12-20 23:57:40 +0900

    Site config 옵션 기능 설정

---
+++ src/config.ts
@@ -0,0 +1,37 @@
+export const SiteConfig = {
+  url: '',
+  pathPrefix: '/',
+  name: 'useonglee.dev',
+  siteTitle: '이우성 기술블로그',
+  title: 'useonglee',
+  subtitle: 'useonglee',
+  copyright: 'useonglee © All rights reserved.',
+  disqusShortname: '',
+  // googleAnalyticsId: '',
+  useKatex: false,
+  menu: [
+    {
+      label: '게시글',
+      path: '/posts/1',
+    },
+    {
+      label: '업무 회고',
+      path: '/review',
+    },
+    {
+      label: '이력서',
+      path: '/about',
+    },
+  ],
+  author: {
+    name: 'useonglee',
+    photo: '/profile.png',
+    bio: 'frontend engineer',
+    contacts: {
+      email: 'useong0830@naver.com',
+      github: 'github.com/useonglee',
+      linkedin: '',
+      instagram: '',
+    },
+  },
+};
