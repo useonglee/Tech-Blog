cc74432846872ef73a88d14dedc24777408b7d15
uslee <uslee@maxst.com>
2021-12-24 23:56:23 +0900

    Layout CSS 수정

--- src/components/layout/layoutStyle.ts
+++ src/components/layout/layoutStyle.ts
@@ -1,8 +1,10 @@
 import styled from 'styled-components';
 
 export const LayoutStyle = styled.div`
-  ${({ theme }) => theme.flex.flexCenter}
-  flex-direction: column;
-  max-width: 48rem;
+  max-width: 62rem;
   margin: auto;
+
+  & > main {
+    padding: 20px 0;
+  }
 `;
