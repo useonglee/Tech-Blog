db2933bf2b9d26c2016c90c30846e61ba5e8738b
uslee <uslee@maxst.com>
2022-01-18 23:19:47 +0900

    Header 컴포넌트 컴파일 에러 테스트

--- src/components/common/Layout/index.tsx
+++ src/components/common/Layout/index.tsx
@@ -1,7 +1,7 @@
 import React, { useCallback, useEffect, useState } from 'react';
 import Head from 'next/head';
 import Link from 'next/link';
-import Header from '@components/Header';
+import Header from '@components/header';
 import Footer from '@components/Footer';
 import { LayoutStyle } from './layoutStyle';
 import { SiteConfig } from '@config';
