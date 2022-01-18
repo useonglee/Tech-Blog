f947f5f58ff786d5eece29bc790d98f9e1b7b8aa
uslee <uslee@maxst.com>
2022-01-18 22:54:24 +0900

    불필요한 코드 제거

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import { GetStaticProps } from 'next';
 import Head from 'next/head';
-import Link from 'next/link';
 import { getSortedPostsData } from '@lib/posts';
-import Date from '@components/common/Date';
 import Layout from '@components/common/Layout';
 import Banner from '@components/common/Banner';
-import { AllPostsDataPropsType, PostDataType } from 'types/postsData';
+import { AllPostsDataPropsType } from 'types/postsData';
 import { SiteConfig } from '@config';
 import PostsList from '@components/common/PostsList';
 
