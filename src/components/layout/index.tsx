8d0c02507e076f9a924519447f3341b5dd0ebc6f
uslee <uslee@maxst.com>
2022-01-22 21:00:09 +0900

    [NO ISSUE] import footer 에러 테스트

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -2,7 +2,7 @@ import React, { useCallback, useEffect, useState } from 'react';
 import Head from 'next/head';
 import Link from 'next/link';
 import Header from '@components/header';
-import Footer from '@components/footer';
+import Footer from '@components/footer/index';
 import { LayoutStyle } from './layoutStyle';
 import { SiteConfig } from '@config';
 
