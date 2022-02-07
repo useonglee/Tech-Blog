f6b29bfe08877b2edda75bfa63ae95396040a35c
uslee <uslee@maxst.com>
2022-02-07 23:35:41 +0900

    document 및 font 링크 위치 수정

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,10 +4,20 @@ import { ThemeProvider as ThemeSwich } from 'next-themes';
 import { ThemeProvider } from 'styled-components';
 import { GlobalStyle } from '@styles/globalStyle';
 import Theme from '@styles/Theme';
+import Head from 'next/head';
 
 function App({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+        <link
+          as="font"
+          rel="stylesheet preload prefetch"
+          href="/font.css"
+          crossOrigin="anonymous"
+        />
+      </Head>
       <GlobalStyle />
       <ThemeSwich>
         <ThemeProvider theme={Theme}>
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -37,20 +37,29 @@ class MyDocument extends Document {
   }
 
   render() {
+    const meta = {
+      title: '이우성 기술블로그',
+      description: "useonglee's tech blog",
+      image:
+        'https://assets.vercel.com/image/upload/q_auto/front/vercel/dps.png',
+    };
+
     return (
       <Html lang="ko">
         <Head>
+          <meta name="robots" content="follow, index" />
+          <meta name="description" content={meta.description} />
           <meta property="og:type" content="blog" />
           <meta property="og:locale" content="ko_KR" />
-          <meta name="description" content="useonglee's tech blog" />
-
-          <link rel="icon" href="/favicon.ico" />
-          <link
-            as="style"
-            rel="stylesheet preload prefetch"
-            href="/font.css"
-            crossOrigin="anonymous"
-          />
+          <meta property="og:site_name" content={meta.title} />
+          <meta property="og:description" content={meta.description} />
+          <meta property="og:title" content={meta.title} />
+          <meta property="og:image" content={meta.image} />
+          <meta name="twitter:card" content="summary_large_image" />
+          <meta name="twitter:site" content={meta.title} />
+          <meta name="twitter:title" content={meta.title} />
+          <meta name="twitter:description" content={meta.description} />
+          <meta name="twitter:image" content={meta.image} />
         </Head>
         <body>
           <Main />
