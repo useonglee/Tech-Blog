3beedb821c56923e285bd6551118edd0285ce94d
uslee <uslee@maxst.com>
2022-02-13 23:57:02 +0900

    fix: imageTag props circle -> radius로 이름 변경

--- src/components/common/imageTag/index.tsx
+++ src/components/common/imageTag/index.tsx
@@ -1,18 +1,18 @@
 import React from 'react';
-import { ImageTagStyle } from './imageTagStyle';
+import { ImageTagStyle } from './style';
 
 interface ImageTagPropsType {
   src: string;
   width: number;
   height: number;
-  circle?: string;
+  radius?: string;
   alt: string;
 }
 
 function ImageTag(props: ImageTagPropsType) {
-  const { circle, ...rest } = props;
+  const { radius, ...rest } = props;
 
-  return <ImageTagStyle priority {...rest} circle={circle} />;
+  return <ImageTagStyle priority {...rest} radius={radius} />;
 }
 
 export default ImageTag;
